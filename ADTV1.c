ac6b49b23188a2e72985bfdc68189283da54d5a1
alifyonanta <28886550+alifyonanta@users.noreply.github.com>
2019-03-14 10:16:04 +0700

    Update ADTV1.c

--- ADTV1.c
+++ ADTV1.c
@@ -8,3 +8,27 @@ struct mahasiswa{
   char grade;
 }a[2];
 
+void cek(int i,int nilai)
+{
+     if (nilai>=85)
+     {
+           a[i].grade='A';
+     }
+     else if(nilai<85||nilai>=75)
+     {
+           a[i].grade='B';
+     }
+     else if(nilai<75||nilai>=65)
+     {
+           a[i].grade='C';
+     }
+     else if(nilai<65||nilai>=55)
+     {
+           a[i].grade='D';
+     }
+     else
+     {
+           a[i].grade='E';
+     }
+
+}
