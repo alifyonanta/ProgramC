6072fb5f984fe45b4ea411ca6d33385243d5b6d6
alifyonanta <28886550+alifyonanta@users.noreply.github.com>
2019-03-15 09:04:25 +0700

    Update ADTV1.c

--- ADTV1.c
+++ ADTV1.c
@@ -25,3 +25,28 @@ void cek(int i,int nilai){
            a[i].grade='E';
      }
 }
+void main(){
+    int i;
+     for(i=0;i<3;i++){
+           printf("\n\nMAHASISWA KE - %d\n",i+1);
+           printf("Masukan nama mahasiswa : ");
+           scanf("%s",&a[i].nama);fflush(stdin);
+
+           do{
+                printf("Masukan nim mahasiswa [harus 10]:");
+                scanf("%s",a[i].nim);fflush(stdin);
+           }while(strlen(a[i].nim)!=10);
+
+           printf("Masukan nilai mahasiswa :");
+           scanf("%d",&a[i].nilai);fflush(stdin);
+           cek(i,a[i].nilai);
+     }
+
+     for(i=0;i<3;i++){
+           printf("\n\nMahasiswa ke %d\n",i+1);
+           printf("Nama : %s\n",a[i].nama);
+           printf("NIM : %s\n",a[i].nim);
+           printf("Nilai : %d\n",a[i].nilai);
+           printf("Grade : %c\n",a[i].grade);
+     }
+}
