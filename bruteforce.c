44de9026be22f21b5ae29d8a81dbf9d0d48cb74a
alifyonanta <28886550+alifyonanta@users.noreply.github.com>
2019-03-18 10:08:07 +0700

    Update bruteforce.c

--- bruteforce.c
+++ bruteforce.c
@@ -3,15 +3,15 @@
 
 struct{
 
-static int iter = 0;    //menghitung jumlah iterasi
-static int done = 0;    //boolean yang menandakan solve telah dicapai
+  static int iter = 0;    //menghitung jumlah iterasi
+  static int done = 0;    //boolean yang menandakan solve telah dicapai
 
 void print(void) { //prosedur untuk mencetak sudoku
 int x, y;
 printf(“———————–\n”);
 for (x = 0; x < 9; x++) {
-for (y = 0; y < 9; y++) {
-printf(“%d “, msudoku[x][y]);
+  for (y = 0; y < 9; y++) {
+    printf(“%d “, msudoku[x][y]);
 if (y % 3 == 2) printf(“| “);
 }
 printf(“\n”);
