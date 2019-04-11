1bbced3bf0dd2126ece74af806d794fa833c39c6
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-04-11 10:47:34 +0700

    Update bruteforce3.c
    add function solveSudoku

--- bruteforce3.c
+++ bruteforce3.c
@@ -21,3 +21,32 @@ int isAvailable(uint8_t puzzle[][9], uint8_t row, uint8_t col, uint8_t num)
     }
     return 1;
 }
+
+int solveSudoku(uint8_t puzzle[][9], uint8_t row, uint8_t col)
+{
+    uint8_t i;
+
+    if(row<9 && col<9)
+    {
+        if(puzzle[row][col])
+        {
+            if((col+1)<9) return solveSudoku(puzzle, row, col+1);
+            else if((row+1)<9) return solveSudoku(puzzle, row+1, 0);
+            else return 1;
+        }
+        else
+        {
+            for(i=0; i<9; ++i)
+            {
+                if(isAvailable(puzzle, row, col, i+1))
+                {
+                    puzzle[row][col] = i+1;
+                    if(solveSudoku(puzzle, row, col)) return 1;
+                    puzzle[row][col] = 0;
+                }
+            }
+        }
+        return 0;
+    }
+    else return 1;
+}
