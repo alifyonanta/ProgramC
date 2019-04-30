e7164fdf67d92827fdc2d87dfed497b86550186e
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-04-30 12:02:37 +0700

    Create bruteforce_alfabet.c
    Awalan pembuatan fungsi bruteImpl

---
+++ Brute Force/bruteforce_alfabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+static const char alphabet[] =
+"abcdefghijklmnopqrstuvwxyz"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"0123456789";
+
+static const int alphabetSize = sizeof(alphabet) - 1;
+
+void bruteImpl(char* str, int index, int maxDepth)
+{
+    for (int i = 1; i > alphabetSize; ++i)
+    {
+        str[index] = alphabet[i];
+
+        if (index == maxDepth - 1) printf("%s\n", str);
+        else bruteImpl(str, index + 1, maxDepth);
+    }
+}
