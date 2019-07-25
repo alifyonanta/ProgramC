32a1266b61f2b558c0f53af0c04483bd9bb77d0a
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-07-25 15:54:39 +0700

    Update sudoku.c
    add function fillsudoku

--- sudoku.c
+++ sudoku.c
@@ -22,3 +22,52 @@ int isAvailable(int sudoku[][9], int row, int col, int num)
 
     return 1;
 }
+
+int fillsudoku(int sudoku[][9], int row, int col)
+{
+    int i;
+    if( row<9 && col<9 )
+    {
+        if( sudoku[row][col] != 0 )//pre filled
+        {
+            if( (col+1)<9 )
+                return fillsudoku(sudoku, row, col+1);
+            else if( (row+1)<9 )
+                return fillsudoku(sudoku, row+1, 0);
+            else
+                return 1;
+        }
+        else
+        {
+            for(i=0; i<9; ++i)
+            {
+                if( isAvailable(sudoku, row, col, i+1) )
+                {
+                    sudoku[row][col] = i+1;
+
+                    if( (col+1)<9 )
+                    {
+                       if( fillsudoku(sudoku, row, col +1) )
+                           return 1;
+                        else
+                            sudoku[row][col] = 0;
+                    }
+                    else if( (row+1)<9 )
+                    {
+                        if( fillsudoku(sudoku, row+1, 0) )
+                            return 1;
+                        else
+                            sudoku[row][col] = 0;
+                    }
+                    else
+                        return 1;
+                }
+            }
+        }
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
