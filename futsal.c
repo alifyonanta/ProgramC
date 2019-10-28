7663315c82117aac2648b3eba3f0374dcc23e724
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-10-28 15:10:04 +0700

    Update futsal.c
    Insert main program

--- futsal.c
+++ futsal.c
@@ -14,3 +14,43 @@ M1 DMember[101];
 L1 DLapangan1[13],DLapangan2[13];
 int pilihanMember,pilihanLapangan,sesi;
 char* statusMember[100];
+
+//Main Program
+int main(){
+
+    printf("Masukkan Nama Anda :\n --> ");
+    scanf("%s",&nama);
+    system("cls");
+    if(CekMember()==0){ //Jika belum menjadi member
+
+        printf("Maaf Anda Belum Menjadi Member\nApakah Mau Menjadi Member ?\n1. Ya\n2. Tidak\n-->");
+        scanf("%i",&pilihanMember);
+        system("cls");
+        if(pilihanMember==1){
+            printf("Masukkan Status Anda\n-->");
+            scanf("%s",&statusMember);
+            system("cls");
+            TambahMember();
+        }else if(pilihanMember==2){
+            cekLapangan();
+            getch();
+            system("cls");
+            printf("Masukkan Jenis Lapangan\n1. Lapangan Vinyl\n2. Lapangan Sintesis\n->");
+            scanf("%i",&pilihanLapangan);
+            printf("Masukkan No Sesi\n->");
+            scanf("%i",&sesi);
+            TambahPesanan();
+        }
+    }else{
+        pilihanMember=1;
+        cekLapangan();
+        getch();
+        system("cls");
+        printf("Masukkan Jenis Lapangan\n1. Lapangan Vinyl\n2. Lapangan Sintesis\n->");
+        scanf("%i",&pilihanLapangan);
+        printf("Masukkan No Sesi\n->");
+        scanf("%i",&sesi);
+        TambahPesanan();
+    }
+
+}
