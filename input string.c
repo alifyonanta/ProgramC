b30b495e4be37de6075bdf5e13cd5398678ba2f5
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-11-08 13:26:59 +0700

    Create input string.c

---
+++ input string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+  char a[50];
+
+  printf ("masukan kalimat : ");
+  scanf (" %49[^\n]",&a);
+  printf ("kalimat anda : %s",a);
+  return 0;
+}
